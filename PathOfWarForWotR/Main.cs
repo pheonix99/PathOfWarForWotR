1362f2980d1c49cebb98dd49f26e830cb969f1da
pheonix99 <infinitum99@gmail.com>
2022-06-26 15:49:33 -0400

    Add project files.

---
+++ PathOfWarForWotR/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+using UnityEngine.UI;
+using HarmonyLib;
+
+namespace PathOfWarForWotR
+{
+    static class Main
+    {
+        public static bool Enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+    }
+}
